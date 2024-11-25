e055cc83f5e6f5c5979a8db47b623ee9fd2a9cb1
nitheeshrl <rlnitheesh@gmail.com>
2024-11-25 23:02:25 +0530

    Update script.js

--- check face/script.js
+++ check face/script.js
@@ -34,7 +34,7 @@ function getLabeledFaceDescriptions() {
     labels.map(async (label) => {
       const descriptions = [];
       for (let i = 1; i <= 2; i++) {
-        const img = await faceapi.fetchImage(`./labels/${label}/${i}.png`);
+        const img = await faceapi.fetchImage(`./labels/${label}/${i}.JPG`);
         const detections = await faceapi
           .detectSingleFace(img)
           .withFaceLandmarks()
