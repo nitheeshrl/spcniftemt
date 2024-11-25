ac81ec9cf1a0d05b14f5116b179145040533a686
nitheeshrl <rlnitheesh@gmail.com>
2024-11-25 22:49:24 +0530

    Create makedir.js

---
+++ makedir.js
@@ -0,0 +1,38 @@
+
+async function main() {
+    var fs = require('fs');
+    const sharp = require("sharp");
+var inames = [
+    'Srivigneswar S',
+    'Sathishwaran S',
+
+       ]
+       var photourls =[
+        'profiles/FSQA - Srivigneshwar.JPG',
+        'profiles/FPE - Satishwaran.JPG'
+         ]
+for(var i =0;i<inames.length;i++){
+
+    var ndir = './user/'+photourls[i];
+    var dir = './labels/'+inames[i];
+    var mdir = './labels/'+inames[i]+'/';
+  /*  fs.mkdirSync(dir, { recursive: true });
+    fs.copyFile(ndir, './labels/'+inames[i]+"/1.JPG", (err) => {
+        if (err) 
+            throw err;
+        console.log('source.txt was copied to destination.txt');
+    });*/
+      try {
+        await sharp (mdir+"1.JPG")
+        .rotate(180) // Rotate the image 90 degrees clockwise
+        .flip() // Flip the image vertically . flop() // Flip the image horizontally
+        .toFile(mdir+"2.JPG") ;
+        console.log("Image midified successfully.");
+        }
+        catch (error) {
+        console.error ("Error modifying image:", error);
+   
+}
+}
+}
+main();
